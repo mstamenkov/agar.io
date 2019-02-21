25d7f1dcbb9bbedac916e14102e34f608ca3759f
Martin Stamenkov <bul2013@abv.bg>
2019-02-21 20:30:29 +0200

    added map grid

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 var myX, myY;
+var x = 1600, y = 1600;
 
 function initGame(){
     //set initial properties of current user
@@ -16,13 +17,28 @@ function update() {
     sharedStorage.setForMe("myX", myX);
     sharedStorage.setForMe("myY", myY);
 }
-
+	
+var p = 0;
 function draw() {
     for (var i=0; i<sharedStorage.list.length; ++i){
         if (sharedStorage.list[i]){
             context.fillRect(sharedStorage.getForUser(i, "myX"), sharedStorage.getForUser(i, "myY"), 30, 30);
         }
     }
+    
+    for (var i = 0; i <= x; i += 50) {
+        context.moveTo(0.5 + i,0);
+        context.lineTo(0.5 + i, y);
+    }
+
+
+    for (var i = 0; i <= y; i += 50) {
+        context.moveTo(p, 0.5+i);
+        context.lineTo(x, 0.5+i);
+    }
+    context.strokeStyle = "#acacac";
+    context.stroke();
+    
 }
 
 function keyup(key) {}
