e4f1507e26843d46e4c03681168edab6e10187d3
sa6oznaka <treildr@gmail.com>
2019-02-22 11:07:46 +0200

    Bug fix + Players are now circles

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 var myX, myY;
 var x = 1600, y = 1600;
+var GridSize = 30;
 
 function initGame(){
     //set initial properties of current user
@@ -20,24 +21,32 @@ function update() {
 	
 var p = 0;
 function draw() {
-    for (var i=0; i<sharedStorage.list.length; ++i){
-        if (sharedStorage.list[i]){
-            context.fillRect(sharedStorage.getForUser(i, "myX"), sharedStorage.getForUser(i, "myY"), 30, 30);
-        }
-    }
     
-    for (var i = 0; i <= x; i += 50) {
+    for (var i = 0; i <= x; i += GridSize) {
         context.moveTo(0.5 + i,0);
         context.lineTo(0.5 + i, y);
     }
 
 
-    for (var i = 0; i <= y; i += 50) {
+    for (var i = 0; i <= y; i += GridSize) {
         context.moveTo(p, 0.5+i);
         context.lineTo(x, 0.5+i);
     }
     context.strokeStyle = "#acacac";
     context.stroke();
+
+    for (var i=0; i<sharedStorage.list.length; ++i){
+        if (sharedStorage.list[i]){
+
+            context.beginPath();
+            context.arc(sharedStorage.getForUser(i, "myX"), sharedStorage.getForUser(i, "myY"), 20, 0, 2 * Math.PI);
+            context.fill();
+            
+            
+        }
+    }
+
+    
     
 }
 
